ad0decc3799ae50c5d7c6e00f6acb7430f9b4105
KingDayveed357 <daveaniago@gmail.com>
2025-07-09 07:06:40 +0100

    fixed vercel deployment errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // import { Toaster } from "@/components/ui/toaster";
-// import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -26,7 +26,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       {/* <Toaster /> */}
-      {/* <Sonner /> */}
+      <Sonner />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
@@ -43,7 +43,7 @@ const App = () => (
           <Route path="/analytics" element={<Layout><VendorAnalytics /></Layout>} />
           <Route path="/rewards" element={<Layout><LoyaltyProgram /></Layout>} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          {/* <Route path="*" element={<NotFound />} /> */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
